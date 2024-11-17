ee3ead78e4b6b847057de5e8a44e3cd4d715ede9
alina3657362 <kostromina.alinochka@mail.ru>
2024-11-17 21:49:57 +0500

    added service and queueservice

---
+++ TgQueueTime/Domain/Services/IService.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Services
+{
+    public interface IQueueService
+    {
+        Task AddClientToQueueAsync(Client client, string organizationName, int windowNumber);
+        Task CreateQueueAsync(Organization organization, int windowNumber);
+        // Другие методы управления очередью
+    }
+
+    public interface IOrganizationService
+    {
+        Task RegisterOrganizationAsync(Organization organization);
+        // Другие методы управления организацией
+    }
+}
---
+++ TgQueueTime/Domain/Services/QueueService.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using Infrastructure.Repositories;
+
+namespace Domain.Services
+{
+    public class QueueService : IQueueService
+    {
+        private readonly IRepository<QueueEntity> _queueRepository;
+        private readonly IRepository<QueueClientsEntity> _clientRepository;
+        private readonly IRepository<OrganizationEntity> _organizationRepository;
+
+        public QueueService(
+            IRepository<QueueEntity> queueRepository,
+            IRepository<QueueClientsEntity> clientRepository,
+            IRepository<OrganizationEntity> organizationRepository)
+        {
+            _queueRepository = queueRepository;
+            _clientRepository = clientRepository;
+            _organizationRepository = organizationRepository;
+        }
+
+        public async Task AddClientToQueueAsync(Client client, string organizationName, int windowNumber)
+        {
+            // добавить клиента в очередь в бд
+        }
+
+        public async Task CreateQueueAsync(Organization organization, int windowNumber)
+        {
+            // добавить очередь в базу данных
+        }
+    }
+
+    public class OrganizationService : IOrganizationService
+    {
+        private readonly IRepository<OrganizationEntity> _organizationRepository;
+
+        public OrganizationService(IRepository<OrganizationEntity> organizationRepository)
+        {
+            _organizationRepository = organizationRepository;
+        }
+
+        public async Task RegisterOrganizationAsync(Organization organization)
+        {
+            var entity = new OrganizationEntity().FromDomain(organization);
+            await _organizationRepository.AddAsync(entity);
+        }
+    }
+}
